66a4d188f08bde156cd7b8f3bb86e2f4716d9d85
dmytropopoilyk <dmytro_popoilyk@epam.com>
2023-01-23 20:18:25 +0200

    change ci workflow

--- hometasks/text_analyser/tests/conftest.py
+++ hometasks/text_analyser/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 import os
 print(os.path.abspath(os.curdir))
-from hometasks.text_analyser.text_analyser import TextAnalyser
+from text_analyser import TextAnalyser
 
 @pytest.fixture(scope='function')
 def text_analyser():
