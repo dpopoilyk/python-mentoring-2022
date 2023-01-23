db65227adfd37937c2993e8950077ea36d706e35
dmytropopoilyk <dmytro_popoilyk@epam.com>
2023-01-23 16:06:34 +0200

    create dockerfile, docker-compose file and terrafoem file

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_ecr_repository" "api_ecr" {
+  name = "contacts-api-repository"
+}
